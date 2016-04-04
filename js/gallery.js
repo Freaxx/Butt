9c519955d318d0e92f8d9fca978113e1f90f31bc
Melissa Lambert <Melissa Lambert>
2016-04-03 21:23:56 -0400

    set up gallery.js variables, function, and event listener

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,20 @@
 //Js File
-
 //Variables
+
+var thumbs = document.querySelectorAll('.thImage'), mainImage = document.querySelector("#bigImage");
 
 //Functions
 
+function changeImage() {
+	//console.log('working');
+	console.log(this.src);
+	//take thumbnail src image name and split it here "_"
+	//this.src
+}
+
 //Event Listeners
+
+//console.log(thumbs.length);
+for (i = 0; i < thumbs.length; i++) {
+	thumbs[i].addEventListener('click',changeImage,false);
+}
